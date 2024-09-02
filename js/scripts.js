ea8b3ef325188085f44fdeb394d125325a7f132c
Devonte Renshaw <devonte@devonterenshaw.com>
2024-09-02 01:38:46 -0700

    Move playRound into playGame function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
-
-playRound();
-playRound();
+playGame();
 
 function getComputerChoice() {
     choice = Math.floor(Math.random() * 3) + 1;
@@ -39,35 +35,46 @@ function getHumanChoice() {
     return choice;
 }
 
-function playRound() {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-     if (checkChoices(humanChoice, computerChoice) == 'tie') {
-        console.log(`It's a tie! We both chose ${humanChoice}.`);
-     } else if (checkChoices(humanChoice, computerChoice) == 'player') {
-        console.log(`Congratulations! You win: ${humanChoice} beats ` +
-            `${computerChoice}!`);
-        humanScore++;
-     } else {
-        console.log(`Too bad! I win: ${humanChoice} loses to ` +
-            `${computerChoice}!`);
-        computerScore++;
-     }
-}
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
 
-function checkChoices(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-        return 'tie';
+    function playRound() {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+    
+         if (checkChoices(humanChoice, computerChoice) == 'tie') {
+            console.log(`It's a tie! We both chose ${humanChoice}.`);
+         } else if (checkChoices(humanChoice, computerChoice) == 'player') {
+            console.log(`Congratulations! You win: ${humanChoice} beats ` +
+                `${computerChoice}!`);
+            humanScore++;
+         } else {
+            console.log(`Too bad! I win: ${humanChoice} loses to ` +
+                `${computerChoice}!`);
+            computerScore++;
+         }
     }
-
-    else if ((humanChoice == "rock" && computerChoice == "scissors") ||
-             (humanChoice == "paper" && computerChoice == "rock") ||
-             (humanChoice == "scissors" && computerChoice == "paper")) {
-        return 'player';
-    }
-
-    else {
-        return 'computer';
+    
+    function checkChoices(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            return 'tie';
+        }
+    
+        else if ((humanChoice == "rock" && computerChoice == "scissors") ||
+                 (humanChoice == "paper" && computerChoice == "rock") ||
+                 (humanChoice == "scissors" && computerChoice == "paper")) {
+            return 'player';
+        }
+    
+        else {
+            return 'computer';
+        }
     }
 }
