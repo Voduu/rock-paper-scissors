5ffb0a58d16e974f78377f464aeecba5fd54de09
Devonte Renshaw <devonte@devonterenshaw.com>
2024-09-02 00:51:13 -0700

    Add getComputerChoice function
    Add function to get a random choice of rock, paper, or scissors for the
    computer's turn

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    choice = Math.floor(Math.random() * 3) + 1;
+
+    switch (choice) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+        default:
+            return "error";
+            break;
+    }
+}
