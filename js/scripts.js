91af33cd15d1cc770a0d0f7a4c12509bf9743f1b
Devonte Renshaw <devonte@devonterenshaw.com>
2024-09-02 01:33:10 -0700

    Add functions to enable playing of a single round
    Add playRound function and checkChoices helper function to enable the
    playing of a single round, including scorekeeping

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+
+playRound();
+playRound();
+
 function getComputerChoice() {
     choice = Math.floor(Math.random() * 3) + 1;
 
@@ -33,5 +39,35 @@ function getHumanChoice() {
     return choice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playRound() {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+
+     if (checkChoices(humanChoice, computerChoice) == 'tie') {
+        console.log(`It's a tie! We both chose ${humanChoice}.`);
+     } else if (checkChoices(humanChoice, computerChoice) == 'player') {
+        console.log(`Congratulations! You win: ${humanChoice} beats ` +
+            `${computerChoice}!`);
+        humanScore++;
+     } else {
+        console.log(`Too bad! I win: ${humanChoice} loses to ` +
+            `${computerChoice}!`);
+        computerScore++;
+     }
+}
+
+function checkChoices(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        return 'tie';
+    }
+
+    else if ((humanChoice == "rock" && computerChoice == "scissors") ||
+             (humanChoice == "paper" && computerChoice == "rock") ||
+             (humanChoice == "scissors" && computerChoice == "paper")) {
+        return 'player';
+    }
+
+    else {
+        return 'computer';
+    }
+}
